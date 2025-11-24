99ef18ac1030dad719f1cda8cb1ef405b268d2f7
Shivam Kagra <shivamkagra38@gmail.com>
2025-11-24 11:25:10 +0530

    Right Chat Container + Login Page

--- Frontend/src/components/LoginPage.js
+++ Frontend/src/components/LoginPage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+
+  const[currState, setCurrState] = useState("Sign up");
+  const[fullName, setFullName] = useState("");
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const[bio, setBio] = useState("");
+
+  const[isDataSubmitted, setIsDataSubmitted] = useState(true);
+
+
   return (
-    <div>
-      L
+    <div className="h-screen flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+
+      <img src={assets.logo_icon} className="w-20"></img>
+
+      <form className="text-white border-2 bg-white/8 border-gray-500 p-8 flex flex-col rounded-lg shadow-lg gap-5 hover:bg-white/10 transition-all ease-in-out ">
+      <div className="text-gray-100 text-2xl flex items-center justify-center">
+        {currState}
+        <img src={assets.arrow_icon} className="w-5"></img>
+      </div>
+      {
+        currState === "Sign up" && !isDataSubmitted && 
+        <input onChange={(e)=>{setFullName(e.target.value)}} type="text" placeholder="Full Name" className=" outline-0 border-b border-gray-500 focus:ring-1 focus:ring-indigo-200 p-1"></input>
+      }
+      {
+        !isDataSubmitted && 
+        <>
+        <input onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder="E-mail" className="outline-0  border-b border-gray-500 focus:ring-1 focus:ring-indigo-200 p-1" required></input>
+        <input onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder="Password" className=" outline-0  border-b border-gray-500 focus:ring-1 focus:ring-indigo-200 p-1" required></input>
+        </>
+      }
+
+      {
+       currState === "Sign up" && isDataSubmitted &&  
+       <textarea onChange={(e)=>{setBio(e.target.value)}} rows="3" className='p-1 border border-gray-500 rounded-md outline-0 focus:ring-1 focus:ring-indigo-200' placeholder="Bio..."></textarea>
+      }
+      
+      <button type="submit">
+        {
+          currState === "Sign up" ? "Create Account" : "Login Now"
+        }
+      </button>
+
+      </form>
+
     </div>
   )
 }
--- Frontend/src/components/RightSideBar.js
+++ Frontend/src/components/RightSideBar.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import assets, { imagesDummyData } from '../assets/assets.js';
 
-const RightSideBar = () => {
-  return (
-    <div>
-      RightSideBar
+const RightSideBar = (props) => {
+
+  const {selectedUser} = props;
+
+  return selectedUser && (
+    <div className={`bg-[#8185B2]/10 basis-3xs text-white relative overflow-y-scroll  ${selectedUser ? "max-md:hidden" : ""}`}>
+      
+      {/* Image Section */}
+      <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
+        
+        <img src={selectedUser?.profilePic || assets.avatar_icon }
+        className="w-15 aspect-square rounded-full"
+        />
+
+        <h1 className="px-10 text-lg font-medium mx-auto flex items-center gap-2 text-center">
+          {selectedUser.fullName}
+        </h1>
+
+        <p className="px-10 mx-auto text-center">{selectedUser.bio}</p>
+
+      </div>
+
+      <div className='m-5 bg-gray-200 h-[1px]'/>
+      {/* Image Section */}
+
+      <div className='px-5 text-xs'>
+        <p>Media</p>
+        <div className="mt-2 max-h-[200px] overflow-y-scroll gap-3 flex flex-wrap opacity-80">
+          {imagesDummyData.map( (url, idx)=> {
+
+            return (
+              <div key={idx} className='cursor-pointer rounded' onClick={()=>{window.open(url)}}>
+                <img src={url} className='max-lg:w-10 lg:w-15 rounded' />
+              </div>
+            );
+
+          })}
+        </div>
+      </div>
+
+      <button className="absolute bg-red-400 bottom-5 left-1/2 -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600
+      text-white border-none text-sm font-light sm:py-2 sm:px-4 lg:py-2 lg:px-8 rounded-full cursor-pointer ">Log out !</button>
+
     </div>
   )
 }
