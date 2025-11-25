56aaace230522e6376d56f7866309286a763ecf7
Shivam Kagra <shivamkagra38@gmail.com>
2025-11-25 20:06:29 +0530

    Profile Page

--- Frontend/src/components/ProfilePage.js
+++ Frontend/src/components/ProfilePage.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets.js';
+import {useNavigate} from "react-router"
 
 const ProfilePage = () => {
+
+  const[selectedImage, setSelectedImage] = useState(null);
+  const navigate = useNavigate();
+  const [name , setName] = useState("Shivam Kagra");
+  const [bio, setBio] = useState("Hello ! I am a MERN developer");
+
+  const submitHandler = async (e) => {
+
+    e.preventDefault();
+    navigate("/");
+
+  }
+
   return (
-    <div>
-      P
+    <div className="h-screen flex items-center justify-center">
+
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+
+        <form onSubmit={submitHandler} className="flex flex-col gap-5 p-10 flex-1 max-sm:w-full">
+
+          <h3 className="text-large">Profile Details</h3>
+
+          <label htmlFor="avtar" className="flex items-center gap-3 cursor pointer">
+            <input onChange={(e)=>{setSelectedImage(e.target.files[0])}} type="file" id="avtar" accept='.jpg, .png, .jpeg' hidden></input>
+            <img src={selectedImage ? URL.createObjectURL(selectedImage) : assets.avatar_icon} className={`w-12 h-12 ${selectedImage && 'rounded-full'}`}></img>
+            Upload Profile Picture
+          </label>
+
+          <input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder="Your Name..." className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" required />
+          <textarea value={bio} onChange={(e)=>{setBio(e.target.value)}} rows={3} placeholder="Your Bio..." className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" required />
+
+            <button type="Submit" className="p-2 bg-gradient-to-r from-purple-400 to-violet-600 rounded-full text-lg cursor-pointer">Save!</button>
+
+        </form>
+
+        <div className='p-10'>
+          <img src={assets.logo_icon} className='sm:w-30 max-sm:w-20  aspect-square'></img>
+        </div>
+        
+
+      </div>
+
     </div>
   )
 }
