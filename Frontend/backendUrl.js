cfeb7bf0adec9fcc2c01925c8b3bf2a934d31798
shivamkagra38 <83065622+shivamkagra38@users.noreply.github.com>
2025-12-10 20:12:00 +0530

    Update backend URL for production environment

--- Frontend/backendUrl.js
+++ Frontend/backendUrl.js
@@ -1,2 +1,3 @@
 //export const backendUrl = "http://localhost:5000";
-export const backendUrl = "https://codecraft-fs-04.onrender.com"
+//export const backendUrl = "https://codecraft-fs-04.onrender.com"
+export const backendUrl = "prolific-charm-production-87c4.up.railway.app"
